ce04314248682e4f7d9d6ed43181c2699647d119
Aizen <aizen@meta.ua>
2015-04-20 17:14:08 +0300

    Add files

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+        TestClass tcObj1 = new TestClass("String 11", "String 12");
+        TestClass tcObj2 = new TestClass("String 21", "String 22");
+        TestClass tcObj3 = new TestClass("String 31", "String 32");
+
+        Method saveMany = null;
+        Method saveOne = null;
+
+        Class<?> cl = TestClass.class;
+        Method[] methods = cl.getMethods();
+        for(Method method: methods){
+            if(method.isAnnotationPresent(Saver.class)){
+                Saver saverAn = method.getAnnotation(Saver.class);
+                if(saverAn.saveMany()){
+                    Object[] params = new TestClass[3];
+                    params[0] = tcObj1;
+                    params[1] = tcObj2;
+                    params[2] = tcObj3;
+                    boolean result = (Boolean) method.invoke(null, new Object[]{params});
+                    System.out.println("Result: "+result);
+                }/*else{
+
+                }*/
+
+            }
+        }
+
+    }
+}
---
+++ src/TestClass.java
@@ -0,0 +1,41 @@
+public class TestClass {
+    @File (name = "/home/aizen/at/s1.txt")
+    private String s1;
+
+    @File (name = "/home/aizen/at/s2.txt")
+    private String s2;
+
+    public TestClass(String str1, String str2){
+        s1 = str1;
+        s2 = str2;
+    }
+
+    public String getS1() {
+        return s1;
+    }
+
+    public String getS2() {
+        return s2;
+    }
+
+    public String toString(){
+        return s1 +" " +s2;
+    }
+
+    @Saver(saveMany = true)
+    public static boolean saveMany(TestClass...tcs){
+        System.out.println("Save many..." + System.lineSeparator());
+        for (TestClass tc: tcs){
+            System.out.println(tc);
+        }
+
+        return  true;
+    }
+
+    @Saver(saveMany = false)
+    public boolean saveOne(){
+        System.out.println("Save one..." + System.lineSeparator());
+        System.out.println(this);
+        return  true;
+    }
+}
